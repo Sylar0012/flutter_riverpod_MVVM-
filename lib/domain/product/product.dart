957ba9caa2774e99260cae5334dbf7687a70091c
syalr0012 <kmc42692@naver.com>
2022-11-15 15:34:03 +0900

    프로젝트 기초 뼈대 완성

---
+++ lib/domain/product/product.dart
@@ -0,0 +1,10 @@
+//스프링에서 주는 방식대로 받아야함
+
+class Product {
+  int id;
+  String name;
+  int price;
+
+  // alt + insert
+  Product(this.id, this.name, this.price);
+}
---
+++ lib/domain/product/product_http_repository.dart
@@ -0,0 +1,21 @@
+class ProductHttpRepository {
+  void findById(int id) {
+    //http 통신코드
+  }
+
+  void findAll() {
+    //http 통신코드
+  }
+
+  void insert() {
+    //http 통신코드
+  }
+
+  void updateById(int id) {
+    //http 통신코드
+  }
+
+  void deleteById(int id) {
+    //http 통신코드
+  }
+}
