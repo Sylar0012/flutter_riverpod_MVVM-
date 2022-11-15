95bfda59fd6f7b3f0c5d6a61ff98647deabcf356
syalr0012 <kmc42692@naver.com>
2022-11-15 17:08:33 +0900

    provider 설정중

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -2,6 +2,12 @@
 // 요청만 함. 응답은 provider가 함
 import 'package:data_app/domain/product/product.dart';
 import 'package:data_app/domain/product/product_http_repository.dart';
+import 'package:data_app/views/product/list/product_list_view_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final productController = Provider((_) {
+  return ProductController();
+});
 
 class ProductController {
   ProductHttpRepository repo = ProductHttpRepository();
--- lib/views/product/list/product_list_page.dart
+++ lib/views/product/list/product_list_page.dart
@@ -1,17 +1,26 @@
+import 'package:data_app/controller/product_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProductListPage extends StatelessWidget {
+class ProductListPage extends ConsumerWidget {
   const ProductListPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pc = ref.read(productController);
+    pc.findAll();
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
       appBar: AppBar(
         title: Text("ProductListPage"),
       ),
       body: ListView.builder(
         itemCount: 5,
         itemBuilder: (context, index) => ListTile(
+          onTap: () {},
           leading: Icon(Icons.account_balance_wallet),
           title: Text(
             "바나나",
